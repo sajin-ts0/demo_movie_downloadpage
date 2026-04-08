8763a1424c4a8ec4be64e4d45c1134239ac08973
AI HUBX Developer <dev@aihubx.local>
2026-04-08 14:14:43 +0530

    Initial commit: AA22xA6 landing page

---
+++ script.js
@@ -0,0 +1,5 @@
+const OFFICIAL_STREAMING_LINK = "https://t.me/+0ufifvfSThVhMGRl";
+
+document.getElementById("downloadButton").addEventListener("click", () => {
+  window.location.href = OFFICIAL_STREAMING_LINK;
+});
